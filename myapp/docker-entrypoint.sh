a8b6d563ea3773f5bb4edb8cd907ae57ff837632
Asad Ullah Aziz <asadullahaziz99@gmail.com>
2023-07-14 00:09:47 +0500

    Feat: debugger setup

---
+++ myapp/docker-entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Check if DEBUG_MODE is set
+if [ "$DEBUG_MODE" = "$SERVICE_NAME" ]; then
+    #  Run Node.js app in debug mode
+    echo "Starting server in debug mode"
+    npm run debug
+else
+    # Run Node.js noramlly
+    echo "Starting server"
+    npm start
