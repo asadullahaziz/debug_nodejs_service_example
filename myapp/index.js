0bf698bb4bb17225d408de86ab8be29eca956b03
Asad Ullah Aziz <asadullahaziz99@gmail.com>
2023-07-13 19:31:45 +0500

    Feat: hello world api

--- myapp/index.js
+++ myapp/index.js
@@ -3,6 +3,10 @@ const express = require("express");
 
 const app = express();
 
+app.get("/", (req, res) => {
+    res.send({"message": "Hello World!"});
+});
+
 
 app.listen(process.env.PORT, () => {
     console.log(`Server started on port ${process.env.PORT}`);
